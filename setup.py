c1ea4c80d3b8c3d6891adf77f60ca1b9aee1f2d6
Matthew Hanson <matt.a.hanson@gmail.com>
2015-07-22 16:02:36 -0400

    basic files

---
+++ setup.py
@@ -0,0 +1,41 @@
+################################################################################
+#    multi-tools: Utilities for using multiprocessing with numpy arrays
+#
+#    AUTHOR: Matthew Hanson
+#    EMAIL:  matt.a.hanson@gmail.com
+#
+#    Copyright (C) 2015 Matthew Hanson
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+################################################################################
+
+from setuptools import setuptools
+
+
+__version__ = imp.load_source('multitools.version', 'multitools/version.py').__version__
+
+
+setup(
+    name='multitools',
+    version=__version__,
+    description='Python utilities for using multiprocessing with numpy arrays',
+    author='Matthew Hanson',
+    author_email='matt.a.hanson@gmail.com',
+    license='Apache v2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=['multitools'],
+)
